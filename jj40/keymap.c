c2601efeb9f7a6a96a52b68957fdd265385b3e1a
rsteube <rsteube@users.noreply.github.com>
2021-11-20 23:47:48 +0100

    added combos

--- jj40/keymap.c
+++ jj40/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_mit( \
   KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC, \
-  KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,  RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, \
+  KC_ESC,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,  RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, \
   KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_ENT,  \
   _______, KC_LCTL,      KC_LALT,      KC_LGUI,      LOWER,        KC_SPC, RAISE, KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT \
 ),
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_mit( \
   KC_TILD, KC_EXLM,       KC_AT,         KC_HASH,       KC_DLR,        KC_PERC, KC_CIRC, KC_AMPR,         KC_ASTR,         KC_LPRN,         KC_RPRN,         KC_BSPC, \
-  KC_DEL,  LGUI_T(KC_F1), LALT_T(KC_F2), LCTL_T(KC_F3), LSFT_T(KC_F4), KC_F5,   KC_F6,   RSFT_T(KC_UNDS), RCTL_T(KC_PLUS), RALT_T(KC_LCBR), RGUI_T(KC_RCBR), KC_PIPE, \
+  KC_DEL,  LGUI_T(KC_F1), LALT_T(KC_F2), LCTL_T(KC_F3), LSFT_T(KC_F4), KC_F5,   KC_F6,   RSFT_T(KC_UNDS), RCTL_T(KC_PLUS), LALT_T(KC_LCBR), RGUI_T(KC_RCBR), KC_PIPE, \
   _______, KC_F7,         KC_F8,         KC_F9,         KC_F10,        KC_F11,  KC_F12,  S(KC_NUHS),      S(KC_NUBS),      _______,         _______,         _______, \
   _______, _______,       _______,       _______,       _______,       _______, _______, KC_MNXT,         KC_VOLD,         KC_VOLU,         KC_MPLY \
 ),
@@ -113,3 +113,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+
+
+const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {LALT_T(KC_S), LCTL_T(KC_D), COMBO_END};
+const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM kl_combo[] = {RCTL_T(KC_K), LALT_T(KC_L), COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(we_combo, KC_TAB),
+    COMBO(sd_combo, KC_ESC),
+    COMBO(io_combo, KC_BSPC),
+    COMBO(kl_combo, KC_ENT),
+};
